467a297d09f59c009b55f8fc2e264c3905944647
dan <dandan682@outlook.com>
2021-08-05 00:53:23 -0500

    update SalaryCalculator

--- SalaryCalculator.java
+++ SalaryCalculator.java
@@ -1,5 +1,18 @@
 package chapter3;
 
 public class SalaryCalculator {
-    
+    public static public static void main(String[] args) {
+        int salary = 1000;
+        int bonus = 250;
+        int quota = 10;
+
+        System.out.println('How many sales did the employee make this week?');
+        Scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+        scanner.close();
+        if(sales > quota) {
+            salary = salary + bonus;
+        }
+        System.out.println("The employee's pay is " + salary);
+    }
 }
