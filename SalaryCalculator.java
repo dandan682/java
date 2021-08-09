e50d5f702bee4b1e521d80b15cca1d81efa145ba
dan <dandan682@outlook.com>
2021-08-08 23:42:42 -0500

    update SalaryCalculator

--- SalaryCalculator.java
+++ SalaryCalculator.java
@@ -5,7 +5,6 @@ public class SalaryCalculator {
         int salary = 1000;
         int bonus = 250;
         int quota = 10;
-
         System.out.println('How many sales did the employee make this week?');
         Scanner = new Scanner(System.in);
         int sales = scanner.nextInt();
