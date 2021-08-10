d51b96c32c0a28d572cf009d36c3df101f3b9466
dan <dandan682@outlook.com>
2021-08-09 23:59:14 -0500

    update QuotaCalculator.java

--- QuotaCalculator.java
+++ QuotaCalculator.java
@@ -1,1 +1,13 @@
 /* if else */
+
+import java.util.Scanner;
+
+public class QuotaCalculator {
+    public static public static void main(String[] args) {
+
+        int quota = 10;
+        System.out.println('Enter the number of sales you made this week?');
+        int sales = scanner.nextInt();
+
+    }
+}
