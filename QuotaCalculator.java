5bc6ea249a770d0315bbf7fdfbf1902773a7e02d
dan <dandan682@outlook.com>
2021-08-10 00:07:13 -0500

    update QuotaCalculator.java

--- QuotaCalculator.java
+++ QuotaCalculator.java
@@ -3,11 +3,19 @@
 import java.util.Scanner;
 
 public class QuotaCalculator {
-    public static public static void main(String[] args) {
-
+    public static void main(String[] args) {
         int quota = 10;
-        System.out.println('Enter the number of sales you made this week?');
+        System.out.println("Enter the number of sales you made this week?");
+        Scanner scanner = new Scanner(System.in);
         int sales = scanner.nextInt();
-
+        scanner.close();
+        if (sales >= quota) {
+            System.out.println("Congrats! You've met your quota");
+        }
+        else {
+            int salesShort = quota - sales;
+            System.out.println("You did not met your quota. You were " + salesShort + 
+                " sales short.");
+        }
     }
 }
