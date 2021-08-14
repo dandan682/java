faf86bd933c6b65b5024038ddf54ecb571ef09f9
dan <dandan682@outlook.com>
2021-08-14 01:08:22 -0500

    update GrandMessage.java

--- GradeMessage.java
+++ GradeMessage.java
@@ -1,15 +1,12 @@
-package chapter3;
-
 import java.util.Scanner;
 
 public class GradeMessage {
     public static void main(String[] args) {
-        int requiredSalary = 30000;
-        int requiredYearsEmployed = 2;
 
         System.out.println("Enter your letter grade:");
         Scanner scanner = new Scanner(System.in);
         String grade = scanner.next();
+        scanner.close();
         String message;
 
         switch (grade) {
@@ -36,5 +33,3 @@ public class GradeMessage {
         System.out.println(message);
     }
 }
-
-
