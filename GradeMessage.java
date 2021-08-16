5b4375ae4c69734abe6f812c77dbd6c567725f54
dan <dandan682@outlook.com>
2021-08-15 23:46:00 -0500

    update GrandMessage.java

--- GradeMessage.java
+++ GradeMessage.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class GradeMessage {
     public static void main(String[] args) {
-
         System.out.println("Enter your letter grade:");
         Scanner scanner = new Scanner(System.in);
         String grade = scanner.next();
