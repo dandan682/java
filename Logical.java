682e77a31c6c57c19f3fa0eadcdeb5e57041ea72
dan <dandan682@outlook.com>
2021-08-17 19:23:51 -0500

    update Logical.java

--- Logical.java
+++ Logical.java
@@ -4,6 +4,23 @@ import java.util.Scanner;
 
 public class Logical {
     public static void main(String[] args) {
-            
+        int requiredSalary = 30000;
+        int requiredYearsEmployed = 2;
+
+        System.out.println("Enter your salary:");
+        Scanner scanner = new Scanner(System.in);
+        double salary = scanner.nextDouble();
+
+        System.out.println("Enter the number of years with your current employer:");
+        double years = scanner.nextDouble();
+        scanner.close();
+
+        if (salary >= requiredSalary && years >= requiredYearsEmployed) {
+                System.out.println("Congrats! You qualify for the loan!");
+            }
+        else {
+            System.out.println("Sorry! You must earn at least $" 
+            + requiredSalary  + " to qualify for the loan.");
         }
     }
+}
